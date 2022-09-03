4289b007c0ebda84da80f6392d05e9ccd32e3562
lmw <alsdndjdrktl@gmail.com>
2022-09-03 10:15:33 +0900

    chore(#11): jest with msw setup
    - env file 수정
    - msw 설치 및 세팅

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
  // Add more setup options before each test is run
- // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+ setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
  moduleDirectories: ['node_modules', '<rootDir>/'],
  testEnvironment: 'jest-environment-jsdom',
@@ -32,6 +32,8 @@ const customJestConfig = {
 
   // Handle module aliases
   '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  '^@components/(.*)$': '<rootDir>/components/$1',
+  '^@hooks/(.*)$': '<rootDir>/hooks/$1',
  },
  globals: {
   'ts-jest': {
@@ -42,3 +44,25 @@ const customJestConfig = {
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
+
+// const nextJest = require('next/jest')
+
+// const createJestConfig = nextJest({
+//  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+//  dir: './',
+// })
+
+// // Add any custom config to be passed to Jest
+// const customJestConfig = {
+//  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//  moduleNameMapper: {
+//   // Handle module aliases (this will be automatically configured for you soon)
+//   '^@/components/(.*)$': '<rootDir>/components/$1',
+
+//   '^@/pages/(.*)$': '<rootDir>/pages/$1',
+//  },
+//  testEnvironment: 'jest-environment-jsdom',
+// }
+
+// // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// module.exports = createJestConfig(customJestConfig)
